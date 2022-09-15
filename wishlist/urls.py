0db335d274223a443ea59b065d70a8a3e73b360b
Muhammad Fauzan Rizky Ramadhan <mfauzan.rizky@gmail.com>
2022-09-15 13:14:25 +0700

    Pengerjaan tutorial 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from wishlist.views import show_wishlist
 
+from wishlist.views import request_xml #Import fungsi yang sudah dibuat di wishlist\view
+from wishlist.views import request_json #Import fungsi yang sudah dibuat di wishlist\view
+
+from wishlist.views import show_json_by_id #Import fungsi yang sudah dibuat di wishlist\view
+
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', request_xml, name='request_xml'),
+    path('json/', request_json, name='show_json'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
+from django.http import HttpResponse
+from django.core import serializers
+
 # Create your views here.
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
@@ -10,3 +13,21 @@ context = {
     'list_barang': data_barang_wishlist,
     'nama': 'M Fauzan Rizky'
 }
+
+
+# Membuat sebuah fungsi yang menerima parameter request (XML)
+def request_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+# Membuat sebuah fungsi yang menerima parameter request (JSON)
+def request_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+# Mengembalikan Data Berdasarkan ID dalam Bentuk JSON/XML
+
+# Membuat sebuah fungsi yang menerima parameter request dan ID (Disini saya menggunakan json)
+def show_json_by_id(request):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
